2659a941d8b0bd757e6d6144f42cb25819977cbb
jcamacaro <jesus.alejandr@gmail.com>
2020-04-10 17:37:57 -0400

    usuario

---
+++ components/user/network.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const response = require('../../network/response')
+const controller = require('./controller')
+
+router.post('/', function (req, res) {  
+    controller.addUser(req.body.name)
+    .then(
+        (data) => {
+            response.succes(req, res, data, 201)
+        }
+    )
+    .catch(
+        (e) => {
+            response.error(req, res, 'Internal error', 500, e)
+        }
+    )
+})
+
+module.exports = router
--- network/routes.js
+++ network/routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const messege = require('../components/message/network')
+const user = require('../components/user/network')
 
 const routes = function(server) {
     server.use('/messege', messege)
+    server.use('/user', user)
 }
 
 module.exports = routes;
