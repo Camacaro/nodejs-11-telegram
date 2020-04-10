e7a5d7ca0935e34f56e487464768aaa579b323ea
jcamacaro <jesus.alejandr@gmail.com>
2020-04-10 14:13:10 -0400

    conexion al a db

--- components/message/store.js
+++ components/message/store.js
@@ -1,18 +1,28 @@
 
+const db = require('mongoose')
+const Model = require('./model')
 
-const list = []
+db.Promise = global.Promise;
+db.connect('mongodb://usertelegrom:usertelegrom123@ds033096.mlab.com:33096/telegrom', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+console.log('[db] conectada con exito');
 
 function addMessage(message) {
-    list.push(message)
+    const myMessage = new Model(message)
+    myMessage.save();
 }
 
-function getMessage() {
-    return list
+async function getMessages() {
+    const messages = await Model.find()
+    return messages
 }
 
 module.exports = {
     add: addMessage,
-    list: getMessage
+    list: getMessages
     // get
     // update
     // delete
